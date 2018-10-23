e216b4408af6b6c7afb83d827b05d08228789f03
Hui Yee <hui.yee.1998@gmail.com>
2018-10-23 22:44:49 +0800

    pushing data to array

--- src/components/cBM_Industries.vue
+++ src/components/cBM_Industries.vue
@@ -1,10 +1,19 @@
 
 <template>
   <div>
+
+    <ul>
+      <li v-for="item in indByMajor"> {{item}} </li>
+    </ul>
+
     <h2> Industries </h2>
-    <input type="text" v-model="searchByMajor" placeholder="Search By Major">
+    <input v-on:keyup = "clearArray" type="text" v-model="searchByMajor" placeholder="Search By Major" >
   <div v-for = "(ind, key) in industry" v-if = "gradsByInd(key, searchByMajor)!==0"> 
-    {{key}}: {{gradsByInd(key, searchByMajor)}} </div>
+    {{addToArray(key, gradsByInd(key, searchByMajor), searchByMajor)}}
+    {{key}}: {{gradsByInd(key, searchByMajor)}} 
+    </div>
+    
+  
 </div>
 
 </template>
@@ -12,10 +21,14 @@
 <script>
 import Firebase from 'firebase'
 import {db} from '../firebase.js';  
+import Vue from 'vue'
   export default {
     data: function(){
       return{
         searchByMajor: '',
+        indByMajor: [],
+        gradz: [],
+        //seen_industries: [],
       }
     },
 
@@ -26,7 +39,8 @@ import {db} from '../firebase.js';
 
       grads(){
         return this.graduates;
-      }
+      },
+
     },
 
     firebase: {
@@ -48,9 +62,24 @@ import {db} from '../firebase.js';
         for (var grad in this.grads){
           if (key === this.grads[grad]["Industry"] && this.grads[grad]["Faculty (First Major)"].toLowerCase()===searchByMajor.toLowerCase()){
             count++;
+            //this.gradz.push(grad);
           }
         }
         return count;
+      },
+
+      addToArray (key, count, searchByMajor){
+        //console.log("KEY", key, "COUNT", count, "MAJOR", searchByMajor)
+        var seen_industries = []
+
+        if (!(seen_industries.includes(key))){
+          //this.indByMajor.push([key, 1])
+          seen_industries.push(key)
+        }
+
+      },
+      clearArray(){
+        this.indByMajor.splice(0);
       }
   }
 }
