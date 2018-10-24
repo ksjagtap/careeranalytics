ce4199af894345c0dcf3cdb1c35af36b4503819a
ksjagtap <ksjagtap@gmail.com>
2018-10-25 00:09:49 +0800

    minor edits

--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ import VueChartkick from 'vue-chartkick'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 import cBM_Industries from './components/cBM_Industries.vue'
+import I_O_overview from './components/I_O_overview.vue'
 
 Vue.config.productionTip = false
 
@@ -17,6 +20,9 @@ Vue.use(VueChartkick, {adapter: Chart});
 Vue.use(VueFire)
 Vue.use(router)
 Vue.use(BootstrapVue)
+
+
+Vue.component('app-I_O_overview', I_O_overview)
 Vue.component('app-cbm_industries', cBM_Industries);
 
 /* eslint-disable no-new */
