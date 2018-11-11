fbdd0167348859482f19fc5497de9cdb5f369288
ksjagtap <ksjagtap@gmail.com>
2018-11-11 14:41:52 +0800

    added nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+
     <img :src="image" alt="NUSCareersLogo" height="45" width="170" align='left'>
     <b-tabs>
       <b-tab title="Profile" active @click="changeRoute('PROFILE')"> </br>
@@ -11,11 +12,55 @@
 
       <b-tab title="Jobs" @click="changeRoute('CAREERS_BY_MAJOR')"> </br>
 
+<!--         <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Page 1</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+          </div>
+      </nav> -->
+
+<!--       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Explore the Possibilities</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+             <router-link to='/careersByMajor'><button type="button" class="button btn-primary">Careers By Major</button></router-link>
+              <router-link to='/industryoutlook'> <button type="button" class="button btn-primary">Industry Outlook</button> </router-link>
+              <router-link to='/comparecompanies'> <button type="button" class="button btn-primary">Compare Companies</button></router-link>
+              <router-link to='/companydetails'> <button type="button" class="button btn-primary">Company Details</button></router-link>
+          </div>
+        </div>
+      </nav> -->
+
+        
+<!--         <nav class="navbar nav-pills navbar-right">
+          <div class="container-fluid">
+              <ul class="nav navbar-nav">
+                <li class="active"><router-link to='/careersByMajor'>Careers by Major</router-link></li>
+                <li><router-link to='/industryoutlook'>Industry Outlook</router-link></li>
+                <li><router-link to='/comparecompanies'>Compare Companies</router-link></li>
+                <li><router-link to='/companydetails'>Company Details</router-link></li>
+            </ul>
+          </div>
+        </nav> -->
+
         <router-link to='/careersByMajor'><button type="button" class="button btn-primary">Careers By Major</button></router-link>
         <router-link to='/industryoutlook'> <button type="button" class="button btn-primary">Industry Outlook</button> </router-link>
         <router-link to='/comparecompanies'> <button type="button" class="button btn-primary">Compare Companies</button> </router-link>
         <router-link to='/companydetails'> <button type="button" class="button btn-primary">Company Details</button></router-link>
+
+
       </b-tab>
+
       <b-tab title="Internship" @click="changeRoute('COMPARE_INTERNSHIPS')"> </br>
         <router-link to='/compareinternships'><button type="button" class="button btn-primary">Compare Internships</button></router-link>
         <router-link to='/internshipdetails'> <button type="button" class="button btn-primary">Internship Details</button></router-link>
